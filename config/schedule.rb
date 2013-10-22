8186311e851371af99ef6f2a560b5e2da38643f1
Atom <fromatom@gmail.com>
2013-10-22 22:52:14 +0900

    write schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,102 @@
+# -*- coding: utf-8 -*-
 set :output, "#{Whenever.path}/log/crontab.log"
 
 every 1.minutes do
   command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
 end
+
+#1限始鈴
+every 1.day, :at => '8:50 am' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#1限終鈴
+every 1.day, :at => '10:20 am' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#2限始鈴
+every 1.day, :at => '10:30 am' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#2限終鈴
+every 1.day, :at => '12:00 am' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#3限始鈴
+every 1.day, :at => '0:50 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#3限終鈴
+every 1.day, :at => '2:20 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#4限始鈴
+every 1.day, :at => '2:30 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#4限終鈴
+every 1.day, :at => '4:00 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#5限始鈴
+every 1.day, :at => '4:10 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#5限終鈴
+every 1.day, :at => '5:40 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#6限始鈴
+every 1.day, :at => '5:50 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#6限終鈴
+every 1.day, :at => '7:20 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#7限始鈴
+every 1.day, :at => '7:30 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#7限終鈴
+every 1.day, :at => '9:00 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
+
+#終電注意
+every 1.day, :at => '10:30 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#終電注意2
+every 1.day, :at => '11:00 pm' do
+  command "cd #{Whenever.path} && mpg123 -b 1024 chime.mp3"
+end
+
+#---
